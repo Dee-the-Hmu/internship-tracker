3b249a27c51c85ba388baf2979671385a279bce8
Dee <aeinhmu.naychi@sjsu.edu>
2026-05-25 16:29:54 -0700

    Plans DAO classes

---
+++ src/main/java/com/dee/internshiptracker/dao/ApplicationDAO.java
@@ -0,0 +1,12 @@
+package com.dee.internshiptracker.dao;
+
+
+public class ApplicationDAO {
+    //get all application 
+    //get an application by id 
+    //create an application (return the generated applicaiton id)
+    //update an application by id 
+    //delete an application 
+
+    //get applications by status (and/or sort by status)
+}
---
+++ src/main/java/com/dee/internshiptracker/dao/CompanyDAO.java
@@ -0,0 +1,12 @@
+package com.dee.internshiptracker.dao;
+
+import model.Company; 
+
+public class CompanyDAO {
+    //get all companies 
+    //get company by id 
+    //create a company (return the generated company id back)
+    //update info about a company by id 
+    //delete a company 
+
+}
